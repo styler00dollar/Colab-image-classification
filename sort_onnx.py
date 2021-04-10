6e0def0d0b89156527b4b2b3ac149b1b45ea3249
styler00dollar <51405565+styler00dollar@users.noreply.github.com>
2021-04-10 08:48:18 +0200

    adding centercrop, softmax, removing model_train arg, replacing torch commands with np

--- sort_onnx.py
+++ sort_onnx.py
@@ -8,12 +8,35 @@ import os
 from tqdm import tqdm
 import numpy as np
 import argparse
+import onnx
+import onnx_tensorrt.backend as backend
+
+"""
+def center_crop(img, dim):
+          width, height = img.shape[1], img.shape[0]	#process crop width and height for max available dimension
+          crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
+          crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0] 
+          mid_x, mid_y = int(width/2), int(height/2)
+          cw2, ch2 = int(crop_width/2), int(crop_height/2) 
+          crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
+          return crop_img
+"""
+def crop_center(img,cropx,cropy):
+          y,x,_ = img.shape
+          startx = x//2-(cropx//2)
+          starty = y//2-(cropy//2)    
+          return img[starty:starty+cropy,startx:startx+cropx]
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=1) # only difference
+
 
 def main():
     # options
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_input_path', type=str, required=True, help='Input folder.')
-    parser.add_argument('--model_train', type=str, required=True)
     parser.add_argument('--onnx_path', type=str, required=True)
     parser.add_argument('--path0', type=str, required=True)
 
@@ -27,28 +50,8 @@ def main():
     args = parser.parse_args()
 
     model = onnx.load(args.onnx_path)
-    engine = backend.prepare(model, device='CUDA:0', int8_mode=True)
-
-
-    if args.model_train == 'efficientnet-b0':
-      model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b1':
-      model = EfficientNet.from_pretrained('efficientnet-b1', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b2':
-      model = EfficientNet.from_pretrained('efficientnet-b2', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b3':
-      model = EfficientNet.from_pretrained('efficientnet-b3', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b4':
-      model = EfficientNet.from_pretrained('efficientnet-b4', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b5':
-      model = EfficientNet.from_pretrained('efficientnet-b5', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b6':
-      model = EfficientNet.from_pretrained('efficientnet-b6', num_classes=args.num_classes)
-    elif args.model_train == 'efficientnet-b7':
-      model = EfficientNet.from_pretrained('efficientnet-b7', num_classes=args.num_classes)
-
-    model.load_state_dict(torch.load(args.model_path, map_location=torch.device('cpu')))
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #engine = backend.prepare(model, device='CUDA:0', int8_mode=True)
+    engine = backend.prepare(model, device='CUDA:0', fp16_mode=True)
 
     if not os.path.exists(args.path0):
         os.makedirs(args.path0)
@@ -58,8 +61,6 @@ def main():
     files = glob.glob(args.data_input_path + '/**/*.png', recursive=True)
     files_jpg = glob.glob(args.data_input_path + '/**/*.jpg', recursive=True)
     files.extend(files_jpg)
-
-    model.to(device)
 
     #height_min = 256
     #width_min = 256
@@ -95,19 +96,24 @@ def main():
                   width_resized = width*scale
                   image = cv2.resize(image, (round(width_resized), round(height_resized)))
 
-          image = torch.from_numpy(image).unsqueeze(0).permute(0,3,1,2)/255
-
-          image=image.to(device)
+          image = crop_center(image, args.width_min, args.height_min)
+          image = np.swapaxes(image,0,2)
+          image = np.expand_dims(image, axis=0)
+          image = image.astype(dtype=np.float32)
 
           y_pred = engine.run(image)[0]
+          #y_pred = engine.run(image)
 
           y_pred = softmax(y_pred)
           top_pred = np.argmax(y_pred, axis=1).astype(np.uint8)
 
-          if top_pred == 1:
-            shutil.move(f, os.path.join(args.path0, os.path.basename(f)))
-          elif top_pred == 2:
-            shutil.move(f, os.path.join(args.path1, os.path.basename(f)))
+          print(f)
+          print(top_pred)
+          print("####################")
+          #if top_pred == 1:
+          #  shutil.move(f, os.path.join(args.path0, os.path.basename(f)))
+          #elif top_pred == 2:
+          #  shutil.move(f, os.path.join(args.path1, os.path.basename(f)))
 
 
 if __name__ == "__main__":
