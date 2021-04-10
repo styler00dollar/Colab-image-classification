c76622fd04752d6f7242334a3566842040201df6
styler00dollar <styler00dollar64@gmail.com>
2021-04-10 03:59:34 +0200

    adding batch_size arg, threaded onnx

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ def main():
     parser.add_argument('--default_root_dir', type=str, required=True)
     parser.add_argument('--means', type=str, nargs='+', required=True)
     parser.add_argument('--std', type=str, nargs='+', required=True)
+    parser.add_argument('--batch_size', type=int, default=8)
 
     parser.add_argument('--model_train', type=str, required=True)
     parser.add_argument('--num_classes', type=int, required=True)
@@ -26,7 +27,7 @@ def main():
     args.means = [float(i) for i in args.means]
     args.std = [float(i) for i in args.std]
 
-    dm = DataModule(training_path=args.training_path, validation_path=args.validation_path, test_path=args.validation_path, num_workers = 16, size = 256, batch_size=1, means=args.means, std=args.std)
+    dm = DataModule(training_path=args.training_path, validation_path=args.validation_path, test_path=args.validation_path, num_workers = 16, size = 256, batch_size=args.batch_size, means=args.means, std=args.std)
     model = CustomTrainClass(model_train=args.model_train, num_classes=args.num_classes, diffaug_activate=args.diffaug_activate, policy=args.policy)
     # skipping validation with limit_val_batches=0
     #gpus=1, limit_val_batches=0,
